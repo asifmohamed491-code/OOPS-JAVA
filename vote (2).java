96c6f0e7a4f163568dcd87cd06803af43369de0c
asifmohamed491-code <asifmohamed491@gmail.com>
2025-08-19 19:28:48 +0530

    created java file

---
+++ vote (2).java
@@ -0,0 +1,13 @@
+class Vote {
+    public static void main(String[] args) {
+        int age = 20; // இங்க நேரா age கொடுத்துட்டோம்
+
+        if (age >= 18) {
+            System.out.println("Eligible to vote ✅");
+        } else {
+            System.out.println("Not eligible to vote ❌");
+        }
+    }
+}
+
+
