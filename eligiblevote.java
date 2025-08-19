a3d20a28ea59fbe6581faf6ce5defa4df8107622
asifmohamed491-code <asifmohamed491@gmail.com>
2025-08-19 19:15:32 +0530

    created java

---
+++ eligiblevote.java
@@ -0,0 +1,15 @@
+public class eligiblevote
+{
+public static void main(String args[])
+{
+int a=17;
+if(a==18)
+{
+System.out.println("eligible for vote");
+}
+else
+{
+System.out.println("not eligible for vote");
+}
+}
+}
---
+++ vote.java
@@ -0,0 +1,19 @@
+public class vote
+{
+public static void main(String args[])
+{
+int a=17;
+if(a==16)
+{
+System.out.println("eligible for vote");
+}
+else if(a==17)
+{
+System.out.println("wait for next year");
+}
+else
+{
+System.out.println("you are a kid");
+}
+}
+}
